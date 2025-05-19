ecace9a90d402ddf421f0f470d79ecb8ee7c0ad1
wuessslima <lim24012@byui.edu>
2025-05-19 15:15:59 -0300

    .

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 
 /* ***********************
@@ -23,9 +24,7 @@ app.set("layout", "./layouts/layout")
  *************************/
 app.use(static)
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/",baseController.buildHome)
 
 /* ***********************
  * Local Server Information
