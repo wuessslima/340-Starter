19c8e2f527a21a5ba5cb19f5dfea80958dde65cf
wuessslima <lim24012@byui.edu>
2025-05-23 17:53:35 -0300

    more bugs

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,11 +2,12 @@
 const express = require("express")
 const router = new express.Router()
 const invController = require("../controllers/invController")
+const Util = require("../utilities")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
-
-module.exports = router;
+router.get("/type/:classificationId", Util.handleErrors(invController.buildByClassificationId));
 
 // Route to build inventory by item view
-router.get("/detail/:inventoryId", utilities.handleErrors(invController.buildByInventoryId));
+router.get("/detail/:inventoryId", Util.handleErrors(invController.buildByInventoryId));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require("./utilities/")
+const Util = require("./utilities/")
 
 /* ***********************
  * View Engine and Templates
